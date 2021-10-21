a45a6cf7d56f5c327d2861d52572ee64a8947234
antcoop <acooper@hireology.com>
2021-10-21 12:52:03 -0500

    removed additional classes

--- client/src/App.js
+++ client/src/App.js
@@ -43,9 +43,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
+        <div>
           <Header />
-          <div className="container">
+          <div>
             <Route exact path="/">
               <Home />
             </Route>
